2f73068bc0d368b4541811e128493359182547ef
Hack Reactor Students <accounts@hackreactor.com>
2016-05-19 12:30:08 -0700

    Did some mongo stuff

--- app/config.js
+++ app/config.js
@@ -1,56 +1,24 @@
 var path = require('path');
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-// var db = mongoose.connect('mongodb://localhost/test');
-
-var knex = require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  },
-  useNullAsDefault: true
-});
-var db = require('bookshelf')(knex);
-
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('baseUrl', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('We\'re connected!');
 });
 
-// exports.Urls = mongoose.model('Urls', {
-//   url: String, 
-//   baseUrl: String, 
-//   code: String, 
-//   title: String, 
-//   visits: Number
-// });
-// exports.Users = mongoose.model('Users', {
-//   username: String, 
-//   password: String
-// });
-
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+db.Urls = mongoose.Schema({
+  url: String, 
+  baseUrl: String, 
+  code: String, 
+  title: String, 
+  visits: Number
 });
+db.Users = mongoose.Schema({
+  username: String, 
+  password: String
+});
+
 
 module.exports = db;
